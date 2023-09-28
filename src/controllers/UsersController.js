86324f69c29ea230c947e750b654c0bf78eb2df3
Vitor <vitorserta@gmail.com>
2023-09-28 18:27:59 -0300

    UserController and database users done

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,26 @@
+const { hash } = require('bcryptjs')
+
+const AppError = require('../utils/AppError')
+
+const sqliteConnection = require('../database/sqlite')
 class UsersController {
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body
 
-    res.status(201).json({ name, email, password })
+    const database = await sqliteConnection()
+    const checkUserExist = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if (checkUserExist) {
+      throw new AppError('This e-mail is already in use')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
+
+    return res.status(201).json()
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
-const express = require('express')
+require('express-async-errors')
+const migrationsRun = require('./database/sqlite/migrations')
+const AppError = require('./utils/AppError')
 
+const express = require('express')
 const routes = require('./routes')
 
+migrationsRun()
 
 const app = express()
 app.use(express.json());
 
 app.use(routes)
 
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
